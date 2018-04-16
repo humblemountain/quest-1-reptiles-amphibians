25e3ec86ca449a9212cfe9e5813eda2e0c542245
Nicholas Gunner <support@getbeyondwords.com>
2018-04-16 05:54:29 -0600

    upload first VR page

---
+++ vr/js/scripts.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', onVrViewLoad);
+
+function onVrViewLoad() {
+  // Selector '#vrview' finds element with id 'vrview'.
+  var vrView = new VRView.Player('#vrview', {
+    image: 'img/blunthead.jpg',
+    is_stereo: false,
+    width: '100%',
+    height: '100%'
+  });
+}
