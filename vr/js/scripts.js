2400a856481061cea2b45ae874dd7232ed97e52b
Nicholas Gunner <support@getbeyondwords.com>
2018-04-16 06:05:31 -0600

    used compressed image

--- vr/js/scripts.js
+++ vr/js/scripts.js
@@ -3,7 +3,7 @@ window.addEventListener('load', onVrViewLoad);
 function onVrViewLoad() {
   // Selector '#vrview' finds element with id 'vrview'.
   var vrView = new VRView.Player('#vrview', {
-    image: 'img/blunthead.jpg',
+    image: 'img/blunthead_compressed.jpg',
     is_stereo: false,
     width: '100%',
     height: '100%'
